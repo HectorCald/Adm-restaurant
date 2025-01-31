6b8f0d4bae61dc137ba37d79754561ad2b1e0e0e
Hector Cald <hi.hector20@gmail.com>
2025-01-31 16:20:54 -0400

    2

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ const restaurantSchema = new mongoose.Schema({
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 const app = express();
 app.use(cors());
 app.use(express.json());
